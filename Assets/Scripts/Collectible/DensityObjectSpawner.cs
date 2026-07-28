1e31a7e106efd25196ea48aef2e8ceea5ee2a010
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-07-28 15:12:54 +0300

    written spawner script

---
+++ Assets/Scripts/Collectible/DensityObjectSpawner.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Fusion;
+using Unity.Mathematics;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Collectible
+{
+    public class DensityObjectSpawner : NetworkBehaviour
+    {
+        [SerializeField] private Transform[] areaCorners;
+        [SerializeField] private float targetDensity, spawnInterval; 
+        [SerializeField, Tooltip("")] private float kernelSize;
+        [SerializeField] private NetworkObject prefabToSpawn;
+        
+        private const int MAX_NUM_FAILS_INITIAL_SPAWN = 1000;
+        private const int MAX_TRACKED_OBJECTS = 1000;
+        // [Networked, Capacity(MAX_TRACKED_OBJECTS)]
+        private List<NetworkObject> SpawnedObjects { get; set; } = default;
+        
+        private bool spawnCoroutineDone = false;
+
+        private void OnValidate()
+        {
+            if (areaCorners == null || areaCorners.Length < 2)
+            {
+                return;
+            }
+
+            if (areaCorners.Length <= 2) return;
+            
+            areaCorners = new Span<Transform>(areaCorners,0,2).ToArray();
+            Debug.LogWarning("Spawner area must only have 2 corners");
+        }
+
+        public override void Spawned()
+        {
+            base.Spawned();
+
+            spawnCoroutineDone = false;
+            StartCoroutine(SpawnSequence());
+        }
+
+        private IEnumerator SpawnObjectsCoroutine(float interval = -1f, int maxNumFails = -1)
+        {
+            Func<int, bool> outOfAttempts = maxNumFails == -1 ? 
+                _ => false :
+                numFails => numFails >= maxNumFails;
+            
+            float kernelVolume = 4f / 3 * math.PI * math.pow(kernelSize, 3);
+            float radiusSquared = math.pow(kernelSize, 2);
+                
+            int numFails = 0;
+            while (!outOfAttempts(numFails))
+            {
+                Vector3 spawnPosition = new Vector3(
+                    Random.Range(areaCorners[0].position.x, areaCorners[1].position.x),
+                    Random.Range(areaCorners[0].position.y, areaCorners[1].position.y),
+                    Random.Range(areaCorners[0].position.z, areaCorners[1].position.z));
+
+                SpawnedObjects.RemoveAll(obj => !obj.IsValid);
+                
+                int numItemsInKernel = SpawnedObjects
+                    .Count(o => (o.transform.position - spawnPosition).sqrMagnitude <= radiusSquared);
+
+                if (numItemsInKernel / kernelVolume >= targetDensity)
+                {
+                    numFails++;
+                }
+                else
+                {
+                    SpawnedObjects.Add(Runner.Spawn(prefabToSpawn, spawnPosition));
+                }
+                
+                yield return interval < 0 ?
+                        null :
+                        new WaitForSeconds(interval);
+            }
+            
+            spawnCoroutineDone = true;
+        }
+
+        private IEnumerator SpawnSequence()
+        {
+            yield return SpawnObjectsCoroutine(maxNumFails: MAX_NUM_FAILS_INITIAL_SPAWN);
+            
+            yield return new WaitUntil(() => spawnCoroutineDone);
+            
+            yield return SpawnObjectsCoroutine(interval: spawnInterval);
+        }
+    }
+}
