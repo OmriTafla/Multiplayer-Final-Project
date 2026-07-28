c83d45917f280c6c7baec5cd23a363c34f6507fc
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-07-28 16:53:16 +0300

    collectible spawner

--- Assets/Scripts/Collectible/DensityObjectSpawner.cs
+++ Assets/Scripts/Collectible/DensityObjectSpawner.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Fusion;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -16,10 +17,10 @@ namespace Collectible
         [SerializeField, Tooltip("")] private float kernelSize;
         [SerializeField] private NetworkObject prefabToSpawn;
         
-        private const int MAX_NUM_FAILS_INITIAL_SPAWN = 1000;
+        private const int MAX_NUM_FAILS = 100;
         private const int MAX_TRACKED_OBJECTS = 1000;
         // [Networked, Capacity(MAX_TRACKED_OBJECTS)]
-        private List<NetworkObject> SpawnedObjects { get; set; } = default;
+        private readonly List<NetworkObject> spawnedObjects = new();
         
         private bool spawnCoroutineDone = false;
 
@@ -44,7 +45,7 @@ namespace Collectible
             StartCoroutine(SpawnSequence());
         }
 
-        private IEnumerator SpawnObjectsCoroutine(float interval = -1f, int maxNumFails = -1)
+        private IEnumerator SpawnObjectsCoroutine(int maxNumFails, float interval, bool tryOnce = false)
         {
             Func<int, bool> outOfAttempts = maxNumFails == -1 ? 
                 _ => false :
@@ -53,31 +54,35 @@ namespace Collectible
             float kernelVolume = 4f / 3 * math.PI * math.pow(kernelSize, 3);
             float radiusSquared = math.pow(kernelSize, 2);
                 
-            int numFails = 0;
-            while (!outOfAttempts(numFails))
+
+            while (true)
             {
-                Vector3 spawnPosition = new Vector3(
-                    Random.Range(areaCorners[0].position.x, areaCorners[1].position.x),
-                    Random.Range(areaCorners[0].position.y, areaCorners[1].position.y),
-                    Random.Range(areaCorners[0].position.z, areaCorners[1].position.z));
-
-                SpawnedObjects.RemoveAll(obj => !obj.IsValid);
-                
-                int numItemsInKernel = SpawnedObjects
-                    .Count(o => (o.transform.position - spawnPosition).sqrMagnitude <= radiusSquared);
-
-                if (numItemsInKernel / kernelVolume >= targetDensity)
+                int numFails = 0;
+                while (!outOfAttempts(numFails))
                 {
-                    numFails++;
+                    Vector3 spawnPosition = new Vector3(
+                        Random.Range(areaCorners[0].position.x, areaCorners[1].position.x),
+                        Random.Range(areaCorners[0].position.y, areaCorners[1].position.y),
+                        Random.Range(areaCorners[0].position.z, areaCorners[1].position.z));
+
+                    spawnedObjects.RemoveAll(obj => !obj.IsValid);
+                    
+                    int numItemsInKernel = spawnedObjects
+                        .Count(o => (o.transform.position - spawnPosition).sqrMagnitude <= radiusSquared);
+
+                    if (numItemsInKernel / kernelVolume >= targetDensity)
+                    {
+                        numFails++;
+                    }
+                    else
+                    {
+                        spawnedObjects.Add(SinglePeer_NetworkRunnerManager.Instance.NetworkRunner.Spawn(prefabToSpawn, spawnPosition));
+                    }
                 }
-                else
-                {
-                    SpawnedObjects.Add(Runner.Spawn(prefabToSpawn, spawnPosition));
-                }
+
+                if (tryOnce) break;
                 
-                yield return interval < 0 ?
-                        null :
-                        new WaitForSeconds(interval);
+                yield return new WaitForSeconds(interval);
             }
             
             spawnCoroutineDone = true;
@@ -85,11 +90,11 @@ namespace Collectible
 
         private IEnumerator SpawnSequence()
         {
-            yield return SpawnObjectsCoroutine(maxNumFails: MAX_NUM_FAILS_INITIAL_SPAWN);
+            yield return SpawnObjectsCoroutine(maxNumFails: MAX_NUM_FAILS, 0f, tryOnce: true);
             
             yield return new WaitUntil(() => spawnCoroutineDone);
             
-            yield return SpawnObjectsCoroutine(interval: spawnInterval);
+            yield return SpawnObjectsCoroutine(maxNumFails: MAX_NUM_FAILS, interval: spawnInterval);
         }
     }
 }
