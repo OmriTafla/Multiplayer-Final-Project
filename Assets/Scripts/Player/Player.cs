4e6481a11cd23346d80730f42137b2d7d4fc9ccf
Eitan <romebattle.net@gmail.com>
2026-07-29 12:55:47 +0300

    Done

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Abb2kTools;
 using DG.Tweening;
 using Fusion;
@@ -8,22 +9,47 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour, IHitable
 {
+    #region Shader Property IDs
     private static readonly int BaseColorId = Shader.PropertyToID("_BaseColor");
     private static readonly int ColorId = Shader.PropertyToID("_Color");
     private static readonly int TintColorId = Shader.PropertyToID("_TintColor");
+    #endregion
 
+    #region Inspector References - Core
     [SerializeField] private Renderer modelRenderer;
     [SerializeField] private Collider hitCollider;
     [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private SpriteRenderer miniMapIconColor;
+    [SerializeField] private CamShakeData hurtShake;
+    #endregion
+
+    #region Inspector References - Owner HUD
     [SerializeField] private Canvas playerUI;
     [SerializeField] private TMP_Text hpLabel;
+    #endregion
+
+    #region Inspector References - Overhead UI
+    [SerializeField] private Canvas overheadUI;
+    [SerializeField] private TMP_Text overheadNicknameLabel;
+    [SerializeField] private TMP_Text overheadHpLabel;
+    [SerializeField] private Transform overheadUIRoot;
+    private Vector3 overheadUIFixedEulerAngles = new Vector3(90f, 0f, 0f);
+    #endregion
+
+    #region Inspector References - Local Movement Prediction
+    [SerializeField] private Transform visualRoot;
+    [SerializeField] private float visualLeadStrength = 1f;
+    [SerializeField] private float visualCatchUpSpeed = 10f;
+    #endregion
+
+    #region Inspector Tunables - Gameplay
     [SerializeField] private float startingHp = 10f;
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float shootingCooldown = 0.5f;
     [SerializeField] private float respawnDelay = 3f;
-    [SerializeField] private SpriteRenderer miniMapIconColor;
-    [SerializeField] private CamShakeData hurtShake;
-    
+    #endregion
+
+    #region Networked State
     [Networked, OnChangedRender(nameof(OnCharacterIdChanged))]
     public int CharacterID { get; private set; }
 
@@ -42,10 +68,12 @@ public class Player : NetworkBehaviour, IHitable
     [Networked] private TickTimer ShootCooldownTimer { get; set; }
     [Networked] private TickTimer RespawnTimer { get; set; }
     [Networked] private NetworkButtons PreviousButtons { get; set; }
-    public int LastFireTick { get; private set; }
-
     [Networked] public Vector3 LastFireDirection { get; private set; }
 
+    public int LastFireTick { get; private set; }
+    #endregion
+
+    #region Private Runtime State
     private CharacterProperties character;
     private string cachedNickname;
     private bool controlsLocalCamera;
@@ -53,20 +81,17 @@ public class Player : NetworkBehaviour, IHitable
     private Renderer[] modelRenderers;
     private Collider[] collisionColliders;
     private TeamsManager teamsManager;
-    
+
     private Vector3 _direction;
     private bool _pendingRespawn;
     private Vector3 _pendingRespawnPosition;
-    
-    [SerializeField] private Transform visualRoot;
-    [SerializeField] private float visualLeadStrength = 1f;
-    [SerializeField] private float visualCatchUpSpeed = 10f;
-
     private Vector3 visualLeadOffset;
+    #endregion
 
+    #region Unity Lifecycle
     private void Update()
     {
-        if (!Object || !Object.HasInputAuthority || IsDead || visualRoot == null)
+        if (!Object || !Object.HasInputAuthority || IsDead || !visualRoot)
             return;
 
         var rawInput = FusionInputProvider.Instance != null
@@ -84,7 +109,71 @@ public class Player : NetworkBehaviour, IHitable
 
         visualRoot.localPosition = visualLeadOffset;
     }
-    
+
+    private void LateUpdate()
+    {
+        if (!overheadUIRoot)
+            return;
+
+        overheadUIRoot.position = transform.position + new Vector3(0, 1, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_pendingRespawn && rigidBody)
+        {
+            rigidBody.position = _pendingRespawnPosition;
+            rigidBody.linearVelocity = Vector3.zero;
+            rigidBody.angularVelocity = Vector3.zero;
+            _pendingRespawn = false;
+        }
+
+        if (rigidBody && !IsDead)
+        {
+            rigidBody.AddForce(_direction * movementSpeed - rigidBody.linearVelocity, ForceMode.VelocityChange);
+        }
+    }
+    #endregion
+
+    #region Fusion Lifecycle
+    public override void Spawned()
+    {
+        CacheReferences();
+        OnCharacterIdChanged();
+
+        if (Object.HasStateAuthority)
+        {
+            Hp = startingHp;
+            IsDead = false;
+            ShootCooldownTimer = TickTimer.None;
+            RespawnTimer = TickTimer.None;
+        }
+
+        StartCoroutine(ResolveNicknameWhenReady());
+
+        if (overheadUIRoot)
+        {
+            overheadUIRoot.SetParent(null, true);
+            overheadUIRoot.rotation = Quaternion.Euler(overheadUIFixedEulerAngles);
+        }
+
+        OnTeamColorChanged();
+        OnHpChanged();
+        OnDeathStateChanged();
+        ConfigureLocalPresentation();
+
+        controlsLocalCamera = Object.HasInputAuthority;
+
+        if (controlsLocalCamera)
+            LocalPlayerCamera.Instance?.SetTarget(transform);
+    }
+
+    public override void Despawned(NetworkRunner runner, bool hasState)
+    {
+        if (controlsLocalCamera)
+            LocalPlayerCamera.Instance?.ClearTarget(transform);
+    }
+
     public override void FixedUpdateNetwork()
     {
         if (!GetInput<GameplayInput>(out var input))
@@ -107,55 +196,9 @@ public class Player : NetworkBehaviour, IHitable
         ProcessAim(input);
         PreviousButtons = input.Buttons;
     }
+    #endregion
 
-    private void FixedUpdate()
-    {
-        if (_pendingRespawn && rigidBody != null)
-        {
-            rigidBody.position = _pendingRespawnPosition;
-            rigidBody.linearVelocity = Vector3.zero;
-            rigidBody.angularVelocity = Vector3.zero;
-            _pendingRespawn = false;
-        }
-
-        if (rigidBody != null && !IsDead)
-        {
-            rigidBody.AddForce(_direction * movementSpeed - rigidBody.linearVelocity, ForceMode.VelocityChange);
-        }
-    }
-
-    public override void Spawned()
-    {
-        CacheReferences();
-        OnCharacterIdChanged();
-
-        if (Object.HasStateAuthority)
-        {
-            Hp = startingHp;
-            IsDead = false;
-            ShootCooldownTimer = TickTimer.None;
-            RespawnTimer = TickTimer.None;
-        }
-
-        cachedNickname = GetNickname();
-
-        OnTeamColorChanged();
-        OnHpChanged();
-        OnDeathStateChanged();
-        ConfigureLocalPresentation();
-
-        controlsLocalCamera = Object.HasInputAuthority;
-
-        if (controlsLocalCamera)
-            LocalPlayerCamera.Instance?.SetTarget(transform);
-    }
-
-    public override void Despawned(NetworkRunner runner, bool hasState)
-    {
-        if (controlsLocalCamera)
-            LocalPlayerCamera.Instance?.ClearTarget(transform);
-    }
-
+    #region Public API
     public void SetCharacter(CharacterProperties newCharacter)
     {
         if (!Object.HasStateAuthority || newCharacter == null)
@@ -202,48 +245,6 @@ public class Player : NetworkBehaviour, IHitable
         return Hp < previousHp;
     }
 
-    private void ProcessAim(GameplayInput input)
-    {
-        var aimPoint = new Vector3(
-            input.AimPosition.x,
-            transform.position.y,
-            input.AimPosition.z);
-
-        var aimDirection = aimPoint - transform.position;
-
-        if (aimDirection.sqrMagnitude > 0.0001f)
-            transform.rotation = Quaternion.LookRotation(aimDirection);
-    }
-
-    private void Move(Vector2 movementInput)
-    {
-        movementInput = Vector2.ClampMagnitude(movementInput, 1f);
-
-        _direction = new Vector3(
-            movementInput.x,
-            0f,
-            movementInput.y);
-    }
-
-    private void ProcessFire(GameplayInput input)
-    {
-        if (!input.Buttons.IsSet(GameplayButton.Fire))
-            return;
-
-        if (!ShootCooldownTimer.ExpiredOrNotRunning(Runner))
-            return;
-
-        ShootCooldownTimer = TickTimer.CreateFromSeconds(Runner, shootingCooldown);
-        LastFireTick = Runner.Tick;
-        LastFireDirection = transform.forward.normalized;
-
-        if (Object.HasStateAuthority)
-        {
-            var placementManager = FindAnyObjectByType<PlacementManager>();
-            placementManager?.SpawnProjectile(Object, transform.position, LastFireDirection);
-        }
-    }
-
     public void OnHit(DamageData data, PlayerRef? hitBy)
     {
         if (!Object.HasStateAuthority || IsDead)
@@ -265,60 +266,53 @@ public class Player : NetworkBehaviour, IHitable
             ApplyBodyFlashRPC();
         }
     }
+    #endregion
 
-    [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority)]
-    void HurtShakeRPC()
+    #region Movement / Aim / Fire
+    private void Move(Vector2 movementInput)
     {
-        CameraShaker.Instance.Shake(hurtShake);
+        movementInput = Vector2.ClampMagnitude(movementInput, 1f);
+
+        _direction = new Vector3(
+            movementInput.x,
+            0f,
+            movementInput.y);
     }
 
-    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
-    void ApplyBodyFlashRPC()
+    private void ProcessAim(GameplayInput input)
     {
-        Color ogColor = TeamColor.a > 0f 
-            ? TeamColor 
-            : (character != null ? character.characterColor : Color.white);
+        var aimPoint = new Vector3(
+            input.AimPosition.x,
+            transform.position.y,
+            input.AimPosition.z);
 
-        foreach (var model in modelRenderers)
-        {
-            DOTween.Kill(model);
+        var aimDirection = aimPoint - transform.position;
 
-            DOTween.Sequence()
-                .Append(DOTween.To(() => ogColor, x => UpdateHitColor(model, x), Color.white, .05f))
-                .Append(DOTween.To(() => Color.white, x => UpdateHitColor(model, x), Color.red, .1f))
-                .Append(DOTween.To(() => Color.red, x => UpdateHitColor(model, x), ogColor, .15f))
-                .SetTarget(model)
-                .SetLink(model.gameObject);
-        }
+        if (aimDirection.sqrMagnitude > 0.0001f)
+            transform.rotation = Quaternion.LookRotation(aimDirection);
     }
 
-    private void UpdateHitColor(Renderer renderer, Color color)
+    private void ProcessFire(GameplayInput input)
     {
-        if (!renderer) return;
-
-        materialPropertyBlock ??= new MaterialPropertyBlock();
-        var materials = renderer.sharedMaterials;
-
-        if (materials.Length == 0)
-        {
-            renderer.GetPropertyBlock(materialPropertyBlock);
-            materialPropertyBlock.SetColor(BaseColorId, color);
-            materialPropertyBlock.SetColor(ColorId, color);
-            materialPropertyBlock.SetColor(TintColorId, color);
-            renderer.SetPropertyBlock(materialPropertyBlock);
+        if (!input.Buttons.IsSet(GameplayButton.Fire))
             return;
-        }
 
-        for (var i = 0; i < materials.Length; i++)
+        if (!ShootCooldownTimer.ExpiredOrNotRunning(Runner))
+            return;
+
+        ShootCooldownTimer = TickTimer.CreateFromSeconds(Runner, shootingCooldown);
+        LastFireTick = Runner.Tick;
+        LastFireDirection = transform.forward.normalized;
+
+        if (Object.HasStateAuthority)
         {
-            renderer.GetPropertyBlock(materialPropertyBlock, i);
-            materialPropertyBlock.SetColor(BaseColorId, color);
-            materialPropertyBlock.SetColor(ColorId, color);
-            materialPropertyBlock.SetColor(TintColorId, color);
-            renderer.SetPropertyBlock(materialPropertyBlock, i);
+            var placementManager = FindAnyObjectByType<PlacementManager>();
+            placementManager?.SpawnProjectile(Object, transform.position, LastFireDirection);
         }
     }
+    #endregion
 
+    #region Death / Respawn
     private void Die()
     {
         if (!Object.HasStateAuthority)
@@ -349,7 +343,37 @@ public class Player : NetworkBehaviour, IHitable
 
         Debug.Log($"{cachedNickname} respawned");
     }
+    #endregion
 
+    #region RPCs - Hit Feedback
+    [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority)]
+    private void HurtShakeRPC()
+    {
+        CameraShaker.Instance.Shake(hurtShake);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void ApplyBodyFlashRPC()
+    {
+        Color ogColor = TeamColor.a > 0f
+            ? TeamColor
+            : (character != null ? character.characterColor : Color.white);
+
+        foreach (var model in modelRenderers)
+        {
+            DOTween.Kill(model);
+
+            DOTween.Sequence()
+                .Append(DOTween.To(() => ogColor, x => UpdateHitColor(model, x), Color.white, .05f))
+                .Append(DOTween.To(() => Color.white, x => UpdateHitColor(model, x), Color.red, .1f))
+                .Append(DOTween.To(() => Color.red, x => UpdateHitColor(model, x), ogColor, .15f))
+                .SetTarget(model)
+                .SetLink(model.gameObject);
+        }
+    }
+    #endregion
+
+    #region Visual Presentation - Model Color / Hit Flash
     private void OnCharacterIdChanged()
     {
         character = CharacterProperties.GetByID(CharacterID);
@@ -406,10 +430,42 @@ public class Player : NetworkBehaviour, IHitable
         }
     }
 
+    private void UpdateHitColor(Renderer renderer, Color color)
+    {
+        if (!renderer) return;
+
+        materialPropertyBlock ??= new MaterialPropertyBlock();
+        var materials = renderer.sharedMaterials;
+
+        if (materials.Length == 0)
+        {
+            renderer.GetPropertyBlock(materialPropertyBlock);
+            materialPropertyBlock.SetColor(BaseColorId, color);
+            materialPropertyBlock.SetColor(ColorId, color);
+            materialPropertyBlock.SetColor(TintColorId, color);
+            renderer.SetPropertyBlock(materialPropertyBlock);
+            return;
+        }
+
+        for (var i = 0; i < materials.Length; i++)
+        {
+            renderer.GetPropertyBlock(materialPropertyBlock, i);
+            materialPropertyBlock.SetColor(BaseColorId, color);
+            materialPropertyBlock.SetColor(ColorId, color);
+            materialPropertyBlock.SetColor(TintColorId, color);
+            renderer.SetPropertyBlock(materialPropertyBlock, i);
+        }
+    }
+    #endregion
+
+    #region Visual Presentation - HP / Death State / UI Visibility
     private void OnHpChanged()
     {
         if (hpLabel)
             hpLabel.text = $"Health: {Hp:F0}";
+
+        if (overheadHpLabel)
+            overheadHpLabel.text = $"{Hp:F0}";
     }
 
     private void OnDeathStateChanged()
@@ -437,6 +493,9 @@ public class Player : NetworkBehaviour, IHitable
         if (playerUI)
             playerUI.gameObject.SetActive(!IsDead && Object.HasInputAuthority);
 
+        if (overheadUI)
+            overheadUI.gameObject.SetActive(!IsDead);
+
         if (controlsLocalCamera && !IsDead)
             LocalPlayerCamera.Instance?.SnapToTarget();
     }
@@ -446,7 +505,50 @@ public class Player : NetworkBehaviour, IHitable
         if (playerUI)
             playerUI.gameObject.SetActive(Object.HasInputAuthority && !IsDead);
     }
+    #endregion
 
+    #region Nickname Logic
+    private IEnumerator ResolveNicknameWhenReady()
+    {
+        string resolved;
+        var attempts = 0;
+
+        do
+        {
+            resolved = GetNickname();
+            attempts++;
+
+            if (!string.IsNullOrEmpty(resolved))
+                break;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+        while (attempts < 50);
+
+        cachedNickname = string.IsNullOrEmpty(resolved)
+            ? $"Player {Object.InputAuthority.PlayerId}"
+            : resolved;
+
+        if (overheadNicknameLabel)
+            overheadNicknameLabel.text = cachedNickname;
+    }
+
+    private string GetNickname()
+    {
+        var dataObject = Runner.GetPlayerObject(Object.InputAuthority);
+
+        if (!dataObject)
+            return $"Player {Object.InputAuthority.PlayerId}";
+
+        var playerData = dataObject.GetComponent<UI.PlayerData>();
+
+        return playerData != null
+            ? playerData.NickName.ToString()
+            : $"Player {Object.InputAuthority.PlayerId}";
+    }
+    #endregion
+
+    #region Shared Helpers
     private void CacheReferences()
     {
         if (modelRenderers == null || modelRenderers.Length == 0)
@@ -463,18 +565,5 @@ public class Player : NetworkBehaviour, IHitable
 
         teamsManager ??= FindAnyObjectByType<TeamsManager>();
     }
-
-    private string GetNickname()
-    {
-        var dataObject = Runner.GetPlayerObject(Object.InputAuthority);
-
-        if (!dataObject)
-            return $"Player {Object.InputAuthority.PlayerId}";
-
-        var playerData = dataObject.GetComponent<UI.PlayerData>();
-
-        return playerData != null
-            ? playerData.NickName.ToString()
-            : $"Player {Object.InputAuthority.PlayerId}";
-    }
+    #endregion
 }
