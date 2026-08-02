4848914988968b12bb5bdfd1e5ee8e3c9e72e5b0
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-08-03 00:57:22 +0300

    upgrade framework

--- Assets/Scripts/Upgrades/PeircingUpgrade.cs
+++ Assets/Scripts/Upgrades/PeircingUpgrade.cs
@@ -1,7 +1,11 @@
+using Enums;
+
 namespace Abb2kTools.Projectiles
 {
     public class PiercingUpgrade : Upgrade<Projectile>
     {
+        public override UpgradeType UpgradeType => UpgradeType.BulletPierce;
+
         public override void ApplyUpgrade(Projectile projectile)
         {
             projectile.piercingLeft++;
