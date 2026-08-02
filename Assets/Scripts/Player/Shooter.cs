8774d94d30392f7cf2a122377675d49e3a8e2ab8
ooooonnnnn <79533443+ooooonnnnn@users.noreply.github.com>
2026-08-02 23:39:45 +0300

    shooter script

---
+++ Assets/Scripts/Player/Shooter.cs
@@ -0,0 +1,52 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using Fusion;
+using UnityEngine;
+
+namespace Abb2kTools.Projectiles
+{
+    public class Shooter : NetworkBehaviour
+    {
+        [Networked] private TickTimer ShootCooldownTimer { get; set; }
+        [Networked] public Vector3 LastFireDirection { get; private set; }
+        public int LastFireTick { get; private set; }
+        [SerializeField] private float shootingCooldown = 0.5f;
+        public HashSet<BulletUpgrade> bulletUpgrades = new();
+    
+        public override void Spawned()
+        {
+            base.Spawned();
+
+            if (Object.HasStateAuthority)
+            {
+                ShootCooldownTimer = TickTimer.None;
+            }
+        }
+
+        public void TryShoot()
+        {
+            if (!Object.HasStateAuthority)
+                return;
+        
+            if (!ShootCooldownTimer.ExpiredOrNotRunning(Runner))
+                return;
+
+            ShootCooldownTimer = TickTimer.CreateFromSeconds(Runner, shootingCooldown);
+            LastFireTick = Runner.Tick;
+            LastFireDirection = transform.forward.normalized;
+
+            if (Object.HasStateAuthority)
+            {
+                var placementManager = FindAnyObjectByType<PlacementManager>();
+                var newProjectile = placementManager?.SpawnProjectile(
+                    Object, transform.position, LastFireDirection);
+
+                foreach (var bulletUpgrade in bulletUpgrades)
+                {
+                    bulletUpgrade.ApplyUpgrade(newProjectile);
+                }
+            }
+        }
+    }
+}
